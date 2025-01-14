4e7245dfe203c2ce8eba36d843d950cb6934d9fe
saira-jerjes <sairajerjes.2@gmail.com>
2025-01-14 20:29:56 +0100

    all done

--- web/src/components/ui/button/button.jsx
+++ web/src/components/ui/button/button.jsx
@@ -1,9 +1,14 @@
 import { Link } from "react-router-dom";
-import './buton.css'
+import "./buton.css";
 
-function Button () {
-    return ( 
-        <Link className="link-underline-opacity-0  m-3 btn rounded rounded-pill" to='personality-test'>Llévame al test</Link>
-    )
+function Button() {
+  return (
+    <Link
+      className="link-underline-opacity-0  m-3 btn rounded rounded-pill"
+      to="personality-test"
+    >
+      Llévame al test
+    </Link>
+  );
 }
-export default Button; 
+export default Button;
